fca8209c0500e40d80dfb171a2cd34976f65588b
Adrian <chopon007@gmail.com>
2022-10-11 20:45:41 +0200

    Ejercicio3 changed

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,31 +1,30 @@
-/******************************************************************************************
- * 3.   Realiza un conversor de pesetas a euros. La cantidad de pesetas que se quiere     *
- *      convertir debe ser introducida por teclado.                                       *
- *                                                                                        *
- ******************************************************************************************
- *                                                                    |  |                *
- *                                                                    |  |                *
- *                          @author Adrián Perogil Fernández        *      *              *
- *                                                                   ******               *
- ******************************************************************************************/
+/*
+    ************************************************************************************
+    *                                                                                  *
+    *         3.   Lectura de datos desde teclado                                      *
+    *                                                                                  *
+    *         3.   Realiza un conversor de pesetas a euros. La cantidad de pesetas     *
+    *              que se quiere convertir debe ser introducida por teclado.           *
+    *                                                                                  *
+    ************************************************************************************
+    *                                                              |  |                *
+    *                                                              |  |                *
+    *                    @author Adrián Perogil Fernández        *      *              *
+    *                                                             ******               *
+    ************************************************************************************
+*/
 import java.util.Scanner;
 
 public class Ejercicio3 {
     public static void main(String[] args) {
-        // Variable: Scanner
+        // Variable Scanner
         Scanner sc = new Scanner(System.in);
-
-        // Input
-        System.out.print("Pesetas: ");
-
-        // Variables: pesetas, euros
+        System.out.print("Introduzca la cantidad de pesetas: ");
+        // Variable "pesetas", "euros"
         int pesetas = sc.nextInt();
         double euros = pesetas / 166.386;
-
         // Output
         System.out.printf("%d pesetas son %.3f euros. \n", pesetas, euros);
-
-        // Scanner closed
         sc.close();
     }
 }
